e70fe4bec51a4aaccb8ca7f9e3f5bb8269ba83ba
Noor Musheer <109195222+NoorMusheer@users.noreply.github.com>
2022-08-25 18:12:36 -0700

    Add files via upload

---
+++ JAVASCRIPT/Consult The Oracle/script.js
@@ -0,0 +1,33 @@
+//Directions: Using the following array, write a function that will answer all of our questions by randomly choosing a response
+
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
+
+function answer(){
+    var choice = Math.floor(Math.random()*lifesAnswers.length);
+    return (lifesAnswers[choice]);
+
+}
+var display = (answer());
+console.log(display);
