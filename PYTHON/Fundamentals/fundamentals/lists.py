d8e5a6a3d17459d05a81a1e52b8436336968dbae
Noor Musheer <109195222+NoorMusheer@users.noreply.github.com>
2022-08-31 20:39:48 -0700

    Add files via upload

--- PYTHON/Fundamentals/fundamentals/lists.py
+++ PYTHON/Fundamentals/fundamentals/lists.py
@@ -25,3 +25,20 @@ def by_length(e):
     return len(e)
 all_i_eat.sort(key=by_length)
 print(all_i_eat)
+
+# splicing
+new_list = ["lets", "see", "how", "well", "this", "works"]
+print(new_list[2:]) #splicing will include the first item given, but not the second. 
+print(new_list[:4]) #this should include values for items at index 0, 1, 2, and 3. 
+print(new_list[1:3])#this should include values for items at index 1 and 2. 
+
+#map is a built-in python function. It will apply a function to each element in a sequence and returns a new iterator. 
+#if i wanted to change all the values in the "all_i_eat" list to a True boolean, then i create a function to do that...
+def make_True(items):
+    items = True
+    return items
+
+#then i create a variable to be set to my new iterable and apply the map function. 
+new_mapped_list = map(make_True,(all_i_eat))
+print(new_mapped_list)
+print(list(new_mapped_list))
